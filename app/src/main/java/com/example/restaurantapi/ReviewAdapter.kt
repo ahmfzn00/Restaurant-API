6f9b4b1a48829104d941a2eb66f9410e32149c80
afzn9x <fauzanjenci009@gmail.com>
2023-03-01 10:28:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/restaurantapi/ReviewAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.restaurantapi
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ReviewAdapter(private val listReview: List<String>) : RecyclerView.Adapter<ReviewAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvItem: TextView = view.findViewById(R.id.tvItem)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_review, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvItem.text = listReview[position]
+    }
+
+    override fun getItemCount() = listReview.size
+
+}
